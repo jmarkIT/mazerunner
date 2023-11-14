ac50c70ae52c0f402cdee1c32ce32d93563a1351
James Mark <james.david.mark@gmail.com>
2023-11-14 16:31:32 -0600

    Refactoring to fix an issue with the Maze class

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from graphics import Window
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(200, 200, 400, 400, win)
-    cell2 = Cell(400, 200, 600, 400, win)
-    cell3 = Cell(400, 400, 600, 600, win)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell3 = Cell(win)
     cell1.has_right_wall = False
     cell2.has_left_wall, cell2.has_bottom_wall = False, False
     cell3.has_top_wall = False
-    cell1.draw()
-    cell2.draw()
-    cell3.draw()
+    cell1.draw(200, 200, 400, 400)
+    cell2.draw(400, 200, 600, 400)
+    cell3.draw(400, 400, 600, 600)
     cell1.draw_move(cell2)
     cell2.draw_move(cell3)
     win.wait_for_close()
