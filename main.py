16c0c106bc5e5ed1d4d1e51a8f46ab82fd12e732
James Mark <james.david.mark@gmail.com>
2023-11-14 18:15:41 -0600

    Drawing the maze rather than individual cells

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from cell import Cell
+from maze import Maze
 from graphics import Window
 
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell3 = Cell(win)
-    cell1.has_right_wall = False
-    cell2.has_left_wall, cell2.has_bottom_wall = False, False
-    cell3.has_top_wall = False
-    cell1.draw(200, 200, 400, 400)
-    cell2.draw(400, 200, 600, 400)
-    cell3.draw(400, 400, 600, 600)
-    cell1.draw_move(cell2)
-    cell2.draw_move(cell3)
+
+    Maze(10, 10, 5, 5, 100, 100, win)
+
     win.wait_for_close()
 
 
